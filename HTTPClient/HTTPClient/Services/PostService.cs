6dad8341000710c350112b615041699bfe82e708
Etec Prof. Horácio Augusto da Silveira <marcospborborema@hotmail.com>
2024-08-08 12:39:35 -0300

    feat: projeto iniciado

--- HTTPClient/HTTPClient/Services/PostService.cs
+++ HTTPClient/HTTPClient/Services/PostService.cs
@@ -2,14 +2,40 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace HTTPClient.Services
 {
     internal class PostService
     {
-        private HttpClient _httpClient;
+        private HttpClient httpClient;
         private List<Post> posts;
+        private JsonSerializerOptions jsonSerializerOptions;
+
+        public PostService()
+        {
+            httpClient = new HttpClient();
+            jsonSerializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true,
+            };
+        }
+        public async Task<List<Post>> GetPostsAsync()
+        {
+            Uri uri = new Uri("https://jsonplaceholder.typicode.com/posts");
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(uri);
+            }
+            catch
+            {
+
+            }
+        }
     }
 }
