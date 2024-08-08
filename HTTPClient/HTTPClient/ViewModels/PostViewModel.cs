bec369cfa7321bd54e3e952b501cc3940c758dcf
Etec Prof. Horácio Augusto da Silveira <marcospborborema@hotmail.com>
2024-08-08 12:36:20 -0300

    feat: projeto iniciado

---
+++ HTTPClient/HTTPClient/ViewModels/PostViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTPClient.ViewModels
+{
+   internal partial class PostViewModel:ObservableObject
+    {
+
+    }
+}
